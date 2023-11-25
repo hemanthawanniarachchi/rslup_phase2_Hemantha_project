98c8cac061a16f81b671c91473a0175889c69a1f
hemanthawanniarachchi <hnwanniarachchi98@gmail.com>
2023-11-25 12:07:48 +0530

    Add forgot password endpoint

--- MyProject/Controllers/AuthController.cs
+++ MyProject/Controllers/AuthController.cs
@@ -1,6 +1,10 @@
 
 using Microsoft.AspNetCore.Mvc;
 using MyProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using MyProject.Models;
+using System;
+using System.Net.Mail;
 
 
 [Route("api/[controller]")]
@@ -35,4 +39,70 @@ public class AuthController : ControllerBase
 
         return Ok(new { user, token });
     }
-}
+
+    [HttpPost("forgotpassword")]
+        public IActionResult ForgotPassword([FromBody] UserLoginModel loginModel)
+        {
+            try
+            {
+                
+                if (!IsValidEmail(loginModel.Username))
+                {
+                    return BadRequest("Invalid email address");
+                }
+
+                // Dummy reset link
+                string resetLink = "https://example.com/reset-password/token123";
+
+                // Dummy email sending
+                SendPasswordResetEmail(loginModel.Username, resetLink);
+
+                return Ok("Password reset link sent successfully");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var mailAddress = new MailAddress(email);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+        private void SendPasswordResetEmail(string email, string resetLink)
+        {
+            // Dummy implementation: Send email using SMTP
+            
+
+            var smtp = new SmtpClient
+            {
+                Host = "",
+                Port = 587,
+                Credentials = new System.Net.NetworkCredential("hemanthawanniarachi@gmail.com", "your-password"),
+                EnableSsl = true
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("hemanthawanniarachi@gmail.com"),
+                Subject = "Password Reset",
+                Body = $"Click the following link to reset your password: {resetLink}",
+                IsBodyHtml = true
+            };
+
+            mailMessage.To.Add(email);
+            smtp.Send(mailMessage);
+
+            
+        }
+    }
+
